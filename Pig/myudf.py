3d240486c9d7b795ab3665740f4d3956b47c267c
subhasis85 <subhtech09@gmail.com>
2018-09-09 02:55:03 +0530

    Update myudf.py

--- Pig/myudf.py
+++ Pig/myudf.py
@@ -3,3 +3,10 @@ def hello_world():
 
 def increase_age(age):
     return age+2
+
+def findvim(line):
+    ind = str(line).find('TwitVim')
+    if(ind != -1):
+        return True
+    else:
+        return False
