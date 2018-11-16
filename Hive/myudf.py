1441c323a6a4d1a04114dee6e68ada8d6b4f34ec
subhasis85 <subhtech09@gmail.com>
2018-11-16 23:08:22 +0530

    Delete myudf.py

--- Hive/myudf.py
+++
@@ -1,8 +0,0 @@
-import sys
-import datetime
-
-for line in sys.stdin:
-    line = line.strip()
-    fname , lname = line.split('\t')
-    l_name = lname.lower()
-    print '\t'.join([fname, str(l_name)])
